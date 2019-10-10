2b677bf010ddc4cda0a1d882db199fc28cb5b4ea
jgarza9788 <jgarza9788@gmail.com>
2019-10-09 21:23:07 -0700

    seems to be cool

--- lib/main.dart
+++ lib/main.dart
@@ -38,41 +38,78 @@ class MyApp extends StatelessWidget {
 }
 
 List<Widget> theseIcons = [
-Container(
-  height: 50.0,
-  width: 50.0,
-  decoration: BD(color:Colors.red),
+GestureDetector(
+  onTap: (){
+    print('Red');
+  },
+  child:   Container(
+    height: 50.0,
+    width: 50.0,
+    decoration: BD(color:Colors.red),
+  ),
 ),
-  Container(
-    height: 50.0,
-    width: 50.0,
-    decoration: BD(color:Colors.orange),
+  GestureDetector(
+    onTap: (){
+      print('Orange');
+    },
+    child: Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: BD(color:Colors.orange),
+    ),
   ),
-  Container(
-    height: 50.0,
-    width: 50.0,
-    decoration: BD(color:Colors.yellow),
+  GestureDetector(
+    onTap: (){
+      print('Yellow');
+    },
+    child: Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: BD(color:Colors.yellow),
+    ),
   ),
-  Container(
-    height: 50.0,
-    width: 50.0,
-    decoration: BD(color:Colors.lightGreen),
+  GestureDetector(
+    onTap: (){
+      print('lightGreen');
+    },
+    child: Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: BD(color:Colors.lightGreen),
+    ),
   ),
-  Container(
-    height: 50.0,
-    width: 50.0,
-    decoration: BD(color:Colors.green),
+  GestureDetector(
+    onTap: (){
+      print('Green');
+    },
+    child: Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: BD(color:Colors.green),
+    ),
   ),
-  Container(
-    height: 50.0,
-    width: 50.0,
-    decoration: BD(color:Colors.teal),
+  GestureDetector(
+    onTap: (){
+      print('Teal');
+    },
+    child: Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: BD(color:Colors.teal),
+    ),
   ),
-  Container(
-    height: 50.0,
-    width: 50.0,
-    decoration: BD(color:Colors.blue),
+  GestureDetector(
+    onTap: (){
+      print('Blue');
+    },
+    child: Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: BD(color:Colors.blue),
+    ),
   ),
+//  Container(),
+//  Container(height: 50.0,width: 50.0,),
 ];
 
 
@@ -104,35 +141,52 @@ class Carousel extends StatefulWidget {
 class _CarouselState extends State<Carousel> {
 
   double shift = 0.0;
+//  double width = 0.0;
+  Size size = Size(1.0, 1.0);
+  double itemOffset = 5.0;
+  GlobalKey _key = GlobalKey();
 
   List<Widget> rebuildWidgetList()
   {
     List<Widget> finalWidgetList = [];
 
-//    for (int i = 0; i < widget.widgetList.length ;i++)
-    for (int i = widget.widgetList.length - 1; i >= 0; i-- )
+    for (int i = 0; i < widget.widgetList.length ;i++)
+//    for (int i = widget.widgetList.length - 1; i >= 0; i-- )
     {
       double ratio = i/widget.widgetList.length;
+      ratio *= itemOffset;
       double negRatio = 1 - ratio;
 
-      print(i);
+//      print(i);
 
-      ratio = ratio + shift;
+//      ratio = ratio + shift;
 
 //      int w = widget.widgetList.length - i -1 ;
 //      print(w);
 
+//      double RS = (ratio+shift)%1.0;
+      double RS = (ratio+shift).clamp(0.0, 1.0);
+      double p = Curves.decelerate.transform(RS);
+
+//      double nRS = (shift)%1.0;
+      double s = 1- Curves.decelerate.flipped.transform(RS);
+      s = 1- Curves.decelerate.flipped.transform(RS);
+//      s = s.clamp(0.0,double.infinity) ;
+
+
       finalWidgetList.add(
         Positioned(
-          left: ratio * 250.0 + 50.0 ,
+//          left: ratio * 250.0 + 50.0 ,
 //          left: (ratio * (0.5 - (ratio * 0.1))) * MediaQuery.of(context).size.width + 50.0,
+//        left:  p * (size.width + 50) + (100.0 * ratio) + 10.0,
+        left:  p * (size.width + 50 ) + (size.width * 0.10 - (0.50 * widget.widgetList.length)) + (size.width * 0.10 * (1/widget.widgetList.length) * ratio) ,
           child: Container(
-            height: 100.0 * negRatio,
-            width: 100.0 * negRatio,
-            child: FittedBox(
-              fit: BoxFit.fitHeight,
+            height: size.height * 0.80 * s ,
+            width: size.width * 0.80 * s ,
+//            child: FittedBox(
+//              fit: BoxFit.fitHeight,
               child: widget.widgetList[i],
-            ),
+//            ),
           ),
         )
       );
@@ -141,28 +195,50 @@ class _CarouselState extends State<Carousel> {
     return finalWidgetList;
   }
 
+
+  _getSize(){
+    RenderBox RB = _key.currentContext.findRenderObject();
+    size = RB.size;
+
+//    print(size);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
     super.initState();
+
     rebuildWidgetList();
+
+  }
+  _afterLayout(_) {
+    _getSize();
   }
 
   @override
   Widget build(BuildContext context) {
+
+    rebuildWidgetList();
+
     return GestureDetector(
       onHorizontalDragUpdate: (value){
         setState(() {
-          shift += value.delta.dx/MediaQuery.of(context).size.width;
+
+          shift += value.delta.dx/size.width;
+          shift = shift.clamp(-1.0 * itemOffset,1.0 * itemOffset);
+
+          print(shift);
         });
-        print('hello');
       },
       child: Container(
+        key: _key,
         height: 250.0,
         width: double.infinity,
         color: Colors.grey[900],
           child: Stack(
-            alignment: AlignmentDirectional.center,
+            overflow: Overflow.clip,
+            alignment: AlignmentDirectional.centerStart,
             children: rebuildWidgetList(),
 //        children: theseIcons,
           ),
