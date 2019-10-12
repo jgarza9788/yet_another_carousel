8c4a4ca5637af16725561c6dfc2b5ea444feef9c
jgarza9788 <jgarza9788@gmail.com>
2019-10-12 15:24:37 -0700

    meh

---
+++ lib/Carousel.dart
@@ -0,0 +1,227 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+
+class Carousel extends StatefulWidget {
+  Carousel({this.widgetList});
+
+  List<Widget> widgetList;
+
+  @override
+  _CarouselState createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+
+  GlobalKey _key = GlobalKey();
+
+  double _shift = 0.0;
+  double _prevDelta = 0.0;
+
+
+  double lastWidgetPosition = 0.0;
+  List<Widget> rebuildWidgetList()
+  {
+    List<Widget> finalWidgetList = [];
+
+    double prevRawPos = 100.0;
+
+
+//    for (int i = 0; i < widget.widgetList.length ;i++)
+    for (int i = widget.widgetList.length - 1; i >= 0; i-- )
+    {
+
+      double ratio = (i/widget.widgetList.length) ;
+      double negRatio = 1.0-ratio;
+      double xRatio = ratio -0.5;
+
+//      ratio -= 0.5;
+//      print('ratio $ratio');
+
+//      print('_size ${_sizeWithExtra.width}');
+
+//      double hwRatio = Curves.easeOutCirc.transform(negRatio);
+////      double hwRatio = sin((negRatio * (pi/2)) + (pi/4));
+//      hwRatio = pow(hwRatio, 10);
+//      hwRatio = hwRatio.clamp(0.0, double.infinity);
+
+
+      double ox = (xRatio * _sizeWithExtra.width + _shift )%_sizeWithExtra.width;
+      ox -= _sizeWithExtra.width/2.0;
+
+      double posX = 0.0;
+      double rawPos = ratio * _sizeWithExtra.width + _shift;
+      posX = rawPos%_sizeWithExtra.width;
+//      print(posX);
+//      print(_size.width);
+//      print(posX/_size.width);
+//      posX = posX%_sizeWithExtra.width;
+//      print(posX);
+
+//      print(
+//          'i $i\nratio $ratio\nnegRatio $negRatio\nxRatio $xRatio\nox $ox\n_shift $_shift\nposX $posX\n'
+//      );
+
+      int insertAt = lastWidgetPosition > posX  ? finalWidgetList.length : 0 ;
+
+//      if (insertAt != 0)
+//        {
+          print(
+              'i $i\nratio $ratio\nnegRatio $negRatio\nxRatio $xRatio\nox $ox\n_shift $_shift\nposX $posX\n'
+          );
+//        }
+
+      if (i == widget.widgetList.length - 1)
+        {
+          lastWidgetPosition = posX;
+        }
+
+      finalWidgetList.insert(
+        insertAt,
+        Transform.translate(
+          offset: Offset(
+              posX,
+              0.0
+          ),
+          child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child:
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text('$i\n${(posX * 100).round()/100}',style: TextStyle(color: Colors.white),),
+                  widget.widgetList[i],
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+//      finalWidgetList.add(
+//        Transform.translate(
+//          offset: Offset(
+////              ox,
+////              ratio * _size.width + _shift,
+////              ratio * _size.width,
+//              posX,
+//              0.0
+//          ),
+//          child: FittedBox(
+//            fit: BoxFit.fitWidth,
+//            child: widget.widgetList[i],
+//          ),
+////          child: Container(
+//////            height: _size.width * 0.5 * ox.abs() ,
+//////            width: _size.width * 0.5 * ox.abs() ,
+//////            height: _size.width * negRatio  *  0.5,
+//////            width: _size.width * negRatio * 0.5,
+////            height: 100.0,
+////            width: 100.0,
+////            child: widget.widgetList[i],
+////          ),
+//        ),
+//      );
+
+
+    }
+
+//    print(finalWidgetList);
+    return finalWidgetList;
+  }
+
+  Size _size = Size.zero;
+  Size _sizeWithExtra = Size.zero;
+  _getSize(){
+    RenderBox RB = _key.currentContext.findRenderObject();
+    _size = RB.size;
+
+//    double w = _size.width * widget.widgetList.length * 0.5 * (( ( 0.5/2 ) + 0.5) * 2);
+//    double w = _size.width * widget.widgetList.length * (( 0.0/2 ) + 0.5) ;
+//    double w = 100.0 * widget.widgetList.length + (0.5 * 100.0 * widget.widgetList.length);//* (( 0.0/2 ) + 0.75) ;
+
+    double unitSize = _size.width * 0.5;
+    double gap = 0.0;
+//    double w = _size.height * widget.widgetList.length + (0.5 * _size.height * widget.widgetList.length);//* (( 0.0/2 ) + 0.75) ;
+    double w = unitSize * widget.widgetList.length + (gap * widget.widgetList.length);
+
+    _sizeWithExtra = Size(w, _size.height);
+    _sizeWithExtra = _size;
+  }
+
+
+    @override
+  void initState() {
+    // TODO: implement initState
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
+    super.initState();
+
+//    shift = (itemOffset/theseIcons.length) * -2;
+    rebuildWidgetList();
+
+//    _controller = AnimationController(
+//      duration: Duration(milliseconds: 500),
+//      vsync: this,
+//    );
+
+//    _controller.addListener((){
+//      setState(() {
+////        print(_controller.value);
+//
+//        print(Curves.easeInOut.transform(1- _controller.value));
+//        prevDelta = prevDelta * Curves.ease.transform(1- _controller.value);
+//        shift += prevDelta;
+//        print('prevDelta $prevDelta');
+//
+////        _onDragEnd();
+////        print(animation.value);
+//      });
+//    });
+  }
+  _afterLayout(_) {
+    _getSize();
+//    rebuildWidgetList();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    rebuildWidgetList();
+
+    return GestureDetector(
+      onHorizontalDragUpdate: (value){
+        setState(() {
+
+          _prevDelta = value.primaryDelta;
+          _shift += _prevDelta;
+//          _shift = _shift.clamp(-1.0, 1.0);
+
+          print(_shift);
+        });
+      },
+//      onTap: (){prevDelta=0.0;},
+//      onHorizontalDragStart: (value){prevDelta=0.0;},
+//      onHorizontalDragEnd: (value){
+//        print('prevDelta $prevDelta');
+//        _controller.duration = Duration(milliseconds: (prevDelta*100000).abs().round());
+//        _controller.forward(from: 0.0);
+//      },
+      child: Container(
+        key: _key,
+        height: 250.0,
+        width: double.infinity,
+        color: Colors.grey[900],
+          child: Stack(
+            overflow: Overflow.clip,
+            alignment: AlignmentDirectional.centerStart,
+            children: rebuildWidgetList(),
+//        children: theseIcons,
+          ),
+      ),
+    );
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
 
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:math' as math;
+import 'Carousel.dart';
+
+import 'friction_simulation.dart';
 
 
 //void main() => runApp(SwipeDemoApp());
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  FrictionSimulation fs = FrictionSimulation(
+    0.1,
+    0.0,
+    0.5,
+  );
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+//    double t = 0.0;
+//    while(t < 1.0)
+//      {
+//        print(fs.x(t));
+//        t += 0.001;
+//      }
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -33,16 +50,6 @@ class MyApp extends StatelessWidget {
               child: Container(
                 color: Colors.red[900],
                 height: 100.0,
-//                child: SwipeButton(
-//                    thumb: Row(
-//                      mainAxisAlignment: MainAxisAlignment.center,
-//                      children: <Widget>[
-//                        Align(widthFactor: 0.33, child: Icon(Icons.chevron_right)),
-//                        Align(widthFactor: 0.33, child: Icon(Icons.chevron_right)),
-//                        Align(widthFactor: 0.33, child: Icon(Icons.chevron_right)),
-//                      ],
-//                    ),
-//                ),
               ),
             ),
           ],
@@ -52,15 +59,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+
+
 List<Widget> theseIcons = [
 GestureDetector(
   onTap: (){
     print('Red');
   },
   child:   Container(
-    height: 50.0,
-    width: 50.0,
+    height: 100.0,
+    width: 100.0,
     decoration: BD(color:Colors.red),
+    child: Text('Red0', style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
   ),
 ),
   GestureDetector(
@@ -68,9 +80,10 @@ GestureDetector(
       print('Orange');
     },
     child: Container(
-      height: 50.0,
-      width: 50.0,
+      height: 100.0,
+      width: 100.0,
       decoration: BD(color:Colors.orange),
+      child: Text('Orange1', style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
     ),
   ),
   GestureDetector(
@@ -78,9 +91,10 @@ GestureDetector(
       print('Yellow');
     },
     child: Container(
-      height: 50.0,
-      width: 50.0,
+      height: 100.0,
+      width: 100.0,
       decoration: BD(color:Colors.yellow),
+      child: Text('Yellow2', style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
     ),
   ),
   GestureDetector(
@@ -88,9 +102,10 @@ GestureDetector(
       print('lightGreen');
     },
     child: Container(
-      height: 50.0,
-      width: 50.0,
+      height: 100.0,
+      width: 100.0,
       decoration: BD(color:Colors.lightGreen),
+      child: Text('LightGreen3', style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
     ),
   ),
   GestureDetector(
@@ -98,9 +113,10 @@ GestureDetector(
       print('Green');
     },
     child: Container(
-      height: 50.0,
-      width: 50.0,
+      height: 100.0,
+      width: 100.0,
       decoration: BD(color:Colors.green),
+      child: Text('Green4', style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
     ),
   ),
   GestureDetector(
@@ -108,9 +124,10 @@ GestureDetector(
       print('Teal');
     },
     child: Container(
-      height: 50.0,
-      width: 50.0,
+      height: 100.0,
+      width: 100.0,
       decoration: BD(color:Colors.teal),
+      child: Text('Teal5', style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
     ),
   ),
   GestureDetector(
@@ -118,9 +135,10 @@ GestureDetector(
       print('Blue');
     },
     child: Container(
-      height: 50.0,
-      width: 50.0,
+      height: 100.0,
+      width: 100.0,
       decoration: BD(color:Colors.blue),
+      child: Text('Blue6', style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
     ),
   ),
 //  Container(),
@@ -132,10 +150,6 @@ BoxDecoration BD({color}){
   return BoxDecoration(
     color: color,
     borderRadius: BorderRadius.circular(10.0),
-//    border: Border.all(
-//      color: Colors.grey[100],
-//      width: 5.0,
-//    ),
     boxShadow: [
       new BoxShadow(
         color: Colors.black.withOpacity(0.25),
@@ -146,197 +160,197 @@ BoxDecoration BD({color}){
   );
 }
 
-class Carousel extends StatefulWidget {
-  Carousel({
-    this.widgetList
-});
+//class Carousel extends StatefulWidget {
+//  Carousel({
+//    this.widgetList
+//});
+//
+//  List<Widget> widgetList;
+//
+//  @override
+//  _CarouselState createState() => _CarouselState();
+//}
 
-  List<Widget> widgetList;
-
-  @override
-  _CarouselState createState() => _CarouselState();
-}
-
-class _CarouselState extends State<Carousel> with SingleTickerProviderStateMixin {
-
-
-  double shift = 0.0;
-//  double width = 0.0;
-  Size size = Size(1.0, 1.0);
-  double itemOffset = 4.0;
-  GlobalKey _key = GlobalKey();
-
-
-  List<Widget> rebuildWidgetList()
-  {
-    List<Widget> finalWidgetList = [];
-
-    for (int i = 0; i < widget.widgetList.length ;i++)
-//    for (int i = widget.widgetList.length - 1; i >= 0; i-- )
-    {
-      double ratio = i/widget.widgetList.length;
-      ratio *= itemOffset;
-      double negRatio = 1 - ratio;
-
-//      print(i);
-
-//      ratio = ratio + shift;
-
-//      int w = widget.widgetList.length - i -1 ;
-//      print(w);
-
-//      double RS = (ratio+shift)%1.0;
-      double RS = (ratio+shift).clamp(0.0, 1.0);
-      double p = Curves.decelerate.transform(RS);
-
-//      double nRS = (shift)%1.0;
-      double s = 1- Curves.decelerate.flipped.transform(RS);
-      s = 1- Curves.decelerate.flipped.transform(RS);
-//      s = s.clamp(0.0,double.infinity) ;
-
-
-      finalWidgetList.add(
-          Transform(
-            transform: Matrix4.rotationY(1-s)..
-//              translate(p * size.width, p * size.height * 0.25)
-              translate(
-                  p * (size.width + 500) + (10.0 * ratio) - 500.0,
-                  p * size.height * 0.25)
-              ..scale(s),
-            child: Container(
-              height: size.height * 0.70 * s ,
-              width: size.width * 0.80 * s ,
-//            child: FittedBox(
-//              fit: BoxFit.fitHeight,
-                child: widget.widgetList[i],
-//            ),
-            ),
-          ),
-      );
-
-
+//class _CarouselState extends State<Carousel> with SingleTickerProviderStateMixin {
+//
+//
+//  double shift = 0.0;
+////  double width = 0.0;
+//  Size size = Size(1.0, 1.0);
+//  double itemOffset = 4.0;
+//  GlobalKey _key = GlobalKey();
+//
+//
+//  List<Widget> rebuildWidgetList()
+//  {
+//    List<Widget> finalWidgetList = [];
+//
+//    for (int i = 0; i < widget.widgetList.length ;i++)
+////    for (int i = widget.widgetList.length - 1; i >= 0; i-- )
+//    {
+//      double ratio = i/widget.widgetList.length;
+//      ratio *= itemOffset;
+//      double negRatio = 1 - ratio;
+//
+////      print(i);
+//
+////      ratio = ratio + shift;
+//
+////      int w = widget.widgetList.length - i -1 ;
+////      print(w);
+//
+////      double RS = (ratio+shift)%1.0;
+//      double RS = (ratio+shift).clamp(0.0, 1.0);
+//      double p = Curves.decelerate.transform(RS);
+//
+////      double nRS = (shift)%1.0;
+//      double s = 1- Curves.decelerate.flipped.transform(RS);
+//      s = 1- Curves.decelerate.flipped.transform(RS);
+////      s = s.clamp(0.0,double.infinity) ;
+//
+//
 //      finalWidgetList.add(
-//        Positioned(
-////          left: ratio * 250.0 + 50.0 ,
-////          left: (ratio * (0.5 - (ratio * 0.1))) * MediaQuery.of(context).size.width + 50.0,
-////        left:  p * (size.width + 50) + (100.0 * ratio) + 10.0,
-//        left:  p * (size.width + 50 ) + (size.width * 0.10 - (0.50 * widget.widgetList.length)) + (size.width * 0.10 * (1/widget.widgetList.length) * ratio) ,
-//          child: Container(
-////            height: size.height * 0.80 * s ,
-////            width: size.width * 0.80 * s ,
-//////            child: FittedBox(
-//////              fit: BoxFit.fitHeight,
-////              child: widget.widgetList[i],
-//////            ),
+//          Transform(
+//            transform: Matrix4.rotationY(1-s)..
+////              translate(p * size.width, p * size.height * 0.25)
+//              translate(
+//                  p * (size.width + 500) + (10.0 * ratio) - 500.0,
+//                  p * size.height * 0.25)
+//              ..scale(s),
+//            child: Container(
+//              height: size.height * 0.70 * s ,
+//              width: size.width * 0.80 * s ,
+////            child: FittedBox(
+////              fit: BoxFit.fitHeight,
+//                child: widget.widgetList[i],
+////            ),
+//            ),
 //          ),
-//        )
 //      );
-    }
-
-    return finalWidgetList;
-  }
-
-
-  _getSize(){
-    RenderBox RB = _key.currentContext.findRenderObject();
-    size = RB.size;
-
-//    print(size);
-  }
-
-  AnimationController _controller;
-  Animation<double> _contentAnimation;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
-    super.initState();
-
-    shift = (itemOffset/theseIcons.length) * -2;
-    rebuildWidgetList();
-
-    _controller = AnimationController(
-      duration: Duration(milliseconds: 500),
-      vsync: this,
-    );
-
-    _controller.addListener((){
-      setState(() {
-//        print(_controller.value);
-
-        print(Curves.easeInOut.transform(1- _controller.value));
-        prevDelta = prevDelta * Curves.ease.transform(1- _controller.value);
-        shift += prevDelta;
-        print('prevDelta $prevDelta');
-
-//        _onDragEnd();
-//        print(animation.value);
-      });
-    });
-  }
-  _afterLayout(_) {
-    _getSize();
-  }
-
-//  double _velocity = 0.0;
-//  _onDragEnd(){
-//    setState(() {
-//      print(_velocity * 0.0005);
-//      shift += Curves.ease.flipped.transform(controller.value) * 0.25 * _velocity.sign ;
-//      shift = shift.clamp(-1.0 * itemOffset, 1.0);
-////      _velocity = _velocity * 0.90;
-//      rebuildWidgetList();
+//
+//
+////      finalWidgetList.add(
+////        Positioned(
+//////          left: ratio * 250.0 + 50.0 ,
+//////          left: (ratio * (0.5 - (ratio * 0.1))) * MediaQuery.of(context).size.width + 50.0,
+//////        left:  p * (size.width + 50) + (100.0 * ratio) + 10.0,
+////        left:  p * (size.width + 50 ) + (size.width * 0.10 - (0.50 * widget.widgetList.length)) + (size.width * 0.10 * (1/widget.widgetList.length) * ratio) ,
+////          child: Container(
+//////            height: size.height * 0.80 * s ,
+//////            width: size.width * 0.80 * s ,
+////////            child: FittedBox(
+////////              fit: BoxFit.fitHeight,
+//////              child: widget.widgetList[i],
+////////            ),
+////          ),
+////        )
+////      );
+//    }
+//
+//    return finalWidgetList;
+//  }
+//
+//
+//  _getSize(){
+//    RenderBox RB = _key.currentContext.findRenderObject();
+//    size = RB.size;
+//
+////    print(size);
+//  }
+//
+//  AnimationController _controller;
+//  Animation<double> _contentAnimation;
+//
+//  @override
+//  void initState() {
+//    // TODO: implement initState
+//    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
+//    super.initState();
+//
+//    shift = (itemOffset/theseIcons.length) * -2;
+//    rebuildWidgetList();
+//
+//    _controller = AnimationController(
+//      duration: Duration(milliseconds: 500),
+//      vsync: this,
+//    );
+//
+//    _controller.addListener((){
+//      setState(() {
+////        print(_controller.value);
+//
+//        print(Curves.easeInOut.transform(1- _controller.value));
+//        prevDelta = prevDelta * Curves.ease.transform(1- _controller.value);
+//        shift += prevDelta;
+//        print('prevDelta $prevDelta');
+//
+////        _onDragEnd();
+////        print(animation.value);
+//      });
 //    });
 //  }
-
-  double prevDelta = 0.0;
-
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    rebuildWidgetList();
-
-    return GestureDetector(
-      onHorizontalDragUpdate: (value){
-        setState(() {
-
-          prevDelta = value.primaryDelta/size.width;
-          shift += prevDelta;
-          shift = shift.clamp(-1.0 * itemOffset, 1.0);
-
-          print(shift);
-        });
-      },
-      onTap: (){prevDelta=0.0;},
-      onHorizontalDragStart: (value){prevDelta=0.0;},
-      onHorizontalDragEnd: (value){
-        print('prevDelta $prevDelta');
-        _controller.duration = Duration(milliseconds: (prevDelta*100000).abs().round());
-        _controller.forward(from: 0.0);
-      },
-      child: Container(
-        key: _key,
-        height: 250.0,
-        width: double.infinity,
-        color: Colors.grey[900],
-          child: Stack(
-            overflow: Overflow.clip,
-            alignment: AlignmentDirectional.center,
-            children: rebuildWidgetList(),
-//        children: theseIcons,
-          ),
-      ),
-    );
-  }
-
-
-}
+//  _afterLayout(_) {
+//    _getSize();
+//  }
+//
+////  double _velocity = 0.0;
+////  _onDragEnd(){
+////    setState(() {
+////      print(_velocity * 0.0005);
+////      shift += Curves.ease.flipped.transform(controller.value) * 0.25 * _velocity.sign ;
+////      shift = shift.clamp(-1.0 * itemOffset, 1.0);
+//////      _velocity = _velocity * 0.90;
+////      rebuildWidgetList();
+////    });
+////  }
+//
+//  double prevDelta = 0.0;
+//
+//
+//  @override
+//  void dispose() {
+//    _controller.dispose();
+//    super.dispose();
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+//    rebuildWidgetList();
+//
+//    return GestureDetector(
+//      onHorizontalDragUpdate: (value){
+//        setState(() {
+//
+//          prevDelta = value.primaryDelta/size.width;
+//          shift += prevDelta;
+//          shift = shift.clamp(-1.0 * itemOffset, 1.0);
+//
+//          print(shift);
+//        });
+//      },
+//      onTap: (){prevDelta=0.0;},
+//      onHorizontalDragStart: (value){prevDelta=0.0;},
+//      onHorizontalDragEnd: (value){
+//        print('prevDelta $prevDelta');
+//        _controller.duration = Duration(milliseconds: (prevDelta*100000).abs().round());
+//        _controller.forward(from: 0.0);
+//      },
+//      child: Container(
+//        key: _key,
+//        height: 250.0,
+//        width: double.infinity,
+//        color: Colors.grey[900],
+//          child: Stack(
+//            overflow: Overflow.clip,
+//            alignment: AlignmentDirectional.center,
+//            children: rebuildWidgetList(),
+////        children: theseIcons,
+//          ),
+//      ),
+//    );
+//  }
+//
+//
+//}
 
